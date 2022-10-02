fced4cc1927815881b91f53d7c7b516a985108e2
Jannis Baum <jannis.baum@gmail.com>
2022-10-02 16:49:50 +0200

    feat: add closet lamp

---
+++ src/closet_lamp.cpp
@@ -0,0 +1,12 @@
+#include "DEV_LampRelay.h"
+#include "HomeSpan.h"
+
+void setup() {
+    Serial.begin(115200);
+    homeSpan.begin(Category::Lighting, "Closet Lamp");
+
+    new SpanAccessory();
+    new Service::AccessoryInformation();
+        new Characteristic::Identify();
+    new DEV_LampRelay(13);
+}
