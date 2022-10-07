c5eed19a1b54530e4d15ed2a9e8c79bc31fb3353
Jannis Baum <jannis.baum@gmail.com>
2022-10-07 18:27:32 +0200

    feat(relay): set default value and inversion

--- include/DEV_LampRelay.h
+++ include/DEV_LampRelay.h
@@ -2,17 +2,23 @@
 
 struct DEV_LampRelay: Service::LightBulb {
     int relayPin;
+    bool invert;
     SpanCharacteristic *state;
     
-    DEV_LampRelay(int pin): Service::LightBulb() {
-        this->state = new Characteristic::On();
+    DEV_LampRelay(int pin, bool defaultOn = false, bool invert = false)
+    : Service::LightBulb()
+    , invert(invert) {
+        this->state = new Characteristic::On(defaultOn);
         this->relayPin = pin;
         pinMode(this->relayPin, OUTPUT);
         digitalWrite(this->relayPin, 1);
     }
     
     boolean update() {
-        digitalWrite(this->relayPin, !this->state->getNewVal());
+        digitalWrite(
+            this->relayPin,
+            this->invert ^ this->state->getNewVal()
+        );
         return (true);
     }  
 };
